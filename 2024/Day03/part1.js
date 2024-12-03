89e94285f8abbfb702bf912d029924400ef64e6e
schmido9 <schmido9@students.zhaw.ch>
2024-12-03 22:08:15 +0100

    refactoring

--- 2024/Day03/part1.js
+++ 2024/Day03/part1.js
@@ -1,7 +1,6 @@
 const fs = require('fs');
 const example = 'xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))';
 const input = fs.readFileSync('input.txt', 'utf8').trim();
-const regex = /mul\((\d{1,}),(\d{1,})\)/gi;
-const result = input.matchAll(regex);
+const result = input.matchAll(/mul\((\d{1,}),(\d{1,})\)/gi);
 const sum = Array.from(result).reduce((acc, [s,a,b] = match) => acc + parseInt(a) * parseInt(b), 0);
 console.log(sum);
--- 2024/Day03/part2.js
+++ 2024/Day03/part2.js
@@ -1,8 +1,7 @@
 const fs = require('fs');
 const example = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
 const input = fs.readFileSync('input.txt', 'utf8').trim();
-const regex = /(?:mul\((?<a>\d{1,}),(?<b>\d{1,})\))|(?<dont>don't\(\))|(?<doit>doit\(\))/gi;
-const result = input.matchAll(regex);
+const result = input.matchAll(/(?:mul\((?<a>\d{1,}),(?<b>\d{1,})\))|(?<dont>don't\(\))|(?<doit>doit\(\))/gi);
 const computation = Array.from(result).reduce((acc, { groups: { a, b, dont, doit }} = match) => {
     if (dont) acc.do = false;
     else if (doit) acc.do = true;
