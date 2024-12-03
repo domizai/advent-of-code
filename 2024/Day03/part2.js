6e7be03c72113a274a0b35a5d442330f5fe0ac63
schmido9 <schmido9@students.zhaw.ch>
2024-12-03 21:43:39 +0100

    refactoring

--- 2024/Day03/part2.js
+++ 2024/Day03/part2.js
@@ -1,11 +1,11 @@
 const fs = require('fs');
 const example = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
 const input = fs.readFileSync('input.txt', 'utf8').trim();
-const regex = /(?:mul\((\d{1,}),(\d{1,})\))|(?:don't\(\))|(?:do\(\))/gi;
+const regex = /(?:mul\((?<a>\d{1,}),(?<b>\d{1,})\))|(?<dont>don't\(\))|(?<doit>doit\(\))/gi;
 const result = input.matchAll(regex);
-const computation = Array.from(result).reduce((acc, [m,a,b] = match) => {
-    if (m === "don't()") acc.do = false;
-    else if (m === "do()") acc.do = true;
+const computation = Array.from(result).reduce((acc, { groups: { a, b, dont, doit }} = match) => {
+    if (dont) acc.do = false;
+    else if (doit) acc.do = true;
     else if (acc.do) acc.sum += parseInt(a) * parseInt(b);
     return acc;
 }, { do: true, sum: 0 });
