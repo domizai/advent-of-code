522428762ba6f83bbe018e6ddc45aeda3d9a2be0
schmido9 <schmido9@students.zhaw.ch>
2024-12-07 20:42:23 +0100

    day 5

--- 2024/Day05/part02.rs
+++ 2024/Day05/part02.rs
@@ -9,30 +9,34 @@ fn main() {
 
     let mut rules: HashMap<i32, Vec<i32>> = HashMap::new();
     for line in rule_tuples.lines() {
-        let rule: Vec<i32> = line.split('|').map(|n| n.trim().parse().expect("Invalid number")).collect();
+        let rule: Vec<i32> = line
+            .split('|')
+            .map(|n| n.trim().parse().expect("Invalid number"))
+            .collect();
+        
         if let [left, right] = *rule.as_slice() {
             rules.entry(left).or_insert_with(Vec::new).push(right);
         }
     }
 
-    let sum: i32 = updates.lines().map(|line| {
-        let mut update: Vec<i32> = line.split(',').map(|n| n.trim().parse().expect("Invalid number")).collect();
-        for i in 1..update.len() {
-            if rules.get(&update[i]).map_or(false, |rights| {
-                rights.iter().any(|&right| update[..i].contains(&right))
-            }) {
-                update.sort_by(|&a, &b| {
-                    if rules.get(&a).map_or(false, |rights| rights.contains(&b)) {
-                        Ordering::Less
-                    } else {
-                        Ordering::Greater
-                    }
-                });
-                return update[(update.len() - 1) / 2];
+    let sum: i32 = updates.lines()
+        .map(|line| {
+            let mut update: Vec<i32> = line
+                .split(',')
+                .map(|n| n.trim().parse().expect("Invalid number"))
+                .collect();
+
+            for i in 1..update.len() {
+                if rules.get(&update[i]).map_or(false, |r| r.iter().any(|&r| update[..i].contains(&r))) {
+                    update.sort_by(|&a, &b| 
+                        rules.get(&a).map_or(false, |r| r.contains(&b))
+                            .then(|| Ordering::Less)
+                            .unwrap_or(Ordering::Greater));
+                    return update[(update.len() - 1) / 2];
+                }
             }
-        }
-        0
-    }).sum();
+            0
+        }).sum();
 
     println!("{}", sum);
 }
