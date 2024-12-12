8c426cf0a39977a23b94b06fe079aaa266d705ea
schmido9 <schmido9@students.zhaw.ch>
2024-12-12 21:41:28 +0100

    day 12

--- 2024/Day12/part01.js
+++ 2024/Day12/part01.js
@@ -20,12 +20,11 @@ const regionCellsMap = {};
 const visited = grid.map(row => row.slice());
 
 const floodfill = (x, y, cell, uid) => {
-    if (visited[y][x] !== cell) return;
+    if (visited[y]?.[x] !== cell) return;
+    visited[y][x] = null;
     if (!regionCellsMap[uid]) regionCellsMap[uid] = [];
     regionCellsMap[uid].push([x, y]);
-    visited[y][x] = null;
     [[x + 1, y], [x - 1, y], [x, y + 1], [x, y - 1]]
-        .filter(([i, j]) => !(i < 0 || i >= rows || j < 0 || j >= cols))
         .forEach(([i, j]) => floodfill(i, j, cell, uid));
 };
 
@@ -38,8 +37,9 @@ for (let y = 0, uid = 0; y < rows; y++) {
 
 // Calculate fence-values for each cell
 const fence = {  
-    top: 1, right: 4, bottom: 2, left: 8,
-    0: 0, 1: 1, 2: 1, 3: 2, 4: 1, 5: 2, 6: 2, 7: 3, 8: 1, 9: 2, 10: 2, 11: 3, 12: 2, 13: 3, 14: 3, 15: 4,    
+    top: 1, bottom: 2, right: 4, left: 8,
+    // fence-value to number of fences per cell
+    0:0, 1:1, 2:1, 3:2, 4:1, 5:2, 6:2, 7:3, 8:1, 9:2, 10:2, 11:3, 12:2, 13:3, 14:3, 15:4,    
 };
 
 const fences = [];
@@ -48,10 +48,10 @@ for (let y = 0; y < rows; y++) {
     for (let x = 0; x < cols; x++) {
         let value = 0;
         const cell = grid[y][x];
-        if (cell !== grid[y][x + 1])   value += fence.right;
-        if (cell !== grid[y + 1]?.[x]) value += fence.bottom;
-        if (cell !== grid[y][x - 1])   value += fence.left;
-        if (cell !== grid[y - 1]?.[x]) value += fence.top;
+        if (cell !== grid[y][x + 1])   value |= fence.right;
+        if (cell !== grid[y + 1]?.[x]) value |= fence.bottom;
+        if (cell !== grid[y][x - 1])   value |= fence.left;
+        if (cell !== grid[y - 1]?.[x]) value |= fence.top;
         fences[y].push(value);
     }
 }
--- 2024/Day12/part02.js
+++ 2024/Day12/part02.js
@@ -1,18 +1,6 @@
 import fs from 'fs';
 
-let input = `
-RRRRIICCFF
-RRRRIICCCF
-VVRRRCCFFF
-VVRCCCJFFF
-VVVVCJJCFE
-VVIVCCJJEE
-VVIIICJJEE
-MIIIIIJJEE
-MIIISIJEEE
-MMMISSJEEE`.trim();
-
-input = fs.readFileSync('input.txt', 'utf8').trim();
+let input = fs.readFileSync('input.txt', 'utf8').trim();
 const grid = input.split('\n').map(row => row.split(''));
 const [cols, rows] = [grid[0].length, grid.length];
 
@@ -21,12 +9,11 @@ const regionCellsMap = {};
 const visited = grid.map(row => row.slice());
 
 const floodfill = (x, y, cell, uid) => {
-    if (visited[y][x] !== cell) return;
+    if (visited[y]?.[x] !== cell) return;
+    visited[y][x] = null;
     if (!regionCellsMap[uid]) regionCellsMap[uid] = [];
     regionCellsMap[uid].push([x, y]);
-    visited[y][x] = null;
     [[x + 1, y], [x - 1, y], [x, y + 1], [x, y - 1]]
-        .filter(([i, j]) => !(i < 0 || i >= rows || j < 0 || j >= cols))
         .forEach(([i, j]) => floodfill(i, j, cell, uid));
 };
 
@@ -39,8 +26,9 @@ for (let y = 0, uid = 0; y < rows; y++) {
 
 // Calculate fence-values for each cell
 const fence = {  
-    t: 1, r: 4, b: 2, l: 8,
-    0: 0, 1: 1, 2: 1, 3: 2, 4: 1, 5: 2, 6: 2, 7: 3, 8: 1, 9: 2, 10: 2, 11: 3, 12: 2, 13: 3, 14: 3, 15: 4,    
+    t:1, b:2, r:4, l:8,
+    // fence-value to number of fences per cell
+    0:0, 1:1, 2:1, 3:2, 4:1, 5:2, 6:2, 7:3, 8:1, 9:2, 10:2, 11:3, 12:2, 13:3, 14:3, 15:4,    
 };
 
 const fences = [];
@@ -68,10 +56,10 @@ Object.entries(regionCellsMap).forEach(([region, cells]) => {
         if (f & fence.r && f & fence.b) c++;
         if (f & fence.l && f & fence.t) c++;
         let n;
-        if ((n = fences[y+1]?.[x-1]) && (n & fence.t && n & fence.r) && !(f & (fence.b | fence.l))) c++;
-        if ((n = fences[y-1]?.[x-1]) && (n & fence.b && n & fence.r) && !(f & (fence.t | fence.l))) c++;
-        if ((n = fences[y+1]?.[x+1]) && (n & fence.t && n & fence.l) && !(f & (fence.b | fence.r))) c++;
-        if ((n = fences[y-1]?.[x+1]) && (n & fence.b && n & fence.l) && !(f & (fence.t | fence.r))) c++;
+        if ((n = fences[y+1]?.[x-1]) && (n & (fence.t | fence.r)) && !(f & (fence.b | fence.l))) c++;
+        if ((n = fences[y-1]?.[x-1]) && (n & (fence.b | fence.r)) && !(f & (fence.t | fence.l))) c++;
+        if ((n = fences[y+1]?.[x+1]) && (n & (fence.t | fence.l)) && !(f & (fence.b | fence.r))) c++;
+        if ((n = fences[y-1]?.[x+1]) && (n & (fence.b | fence.l)) && !(f & (fence.t | fence.r))) c++;
     });
     regionCornersMap[region] = c;
 });
